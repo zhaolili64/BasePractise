a77010d7ee9067cdeb2034bf8501b745770ccf71
zhaolili64 <71702057+zhaolili64@users.noreply.github.com>
2020-10-05 15:26:07 +0800

    Kernighan

---
+++ Kernighan.java
@@ -0,0 +1,11 @@
+public class Kernighan {
+    public static void main(String[] args) {
+        int sum =0 ;
+        int i = 0 ;
+        int j= 0;
+        for(i = 1 ;i <= 100 ; i++){
+            sum = sum + i;
+            System.out.println(sum);
+}
+}
+}
